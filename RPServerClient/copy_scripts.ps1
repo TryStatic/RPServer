de51eee423ba34ae6bf1cb178351c74531abf812
Static <jimakosfb@gmail.com>
2019-05-27 18:30:37 +0300

    Made copy_scripts.ps1 ensure the cs_packages directory gets created.

--- RPServerClient/copy_scripts.ps1
+++ RPServerClient/copy_scripts.ps1
@@ -1,2 +1,3 @@
-﻿Remove-Item -Path $PSScriptRoot\..\client_packages\cs_packages\* -Recurse
+﻿New-Item -ItemType Directory -Force -Path $PSScriptRoot\..\client_packages\cs_packages *>&1 | out-null
+Remove-Item -Path $PSScriptRoot\..\client_packages\cs_packages\* -Recurse
 get-childitem -path $PSScriptRoot -exclude obj, bin | Get-Childitem -recurse | Where-Object {$_.Extension -eq ".cs"} | Copy-Item -Destination $PSScriptRoot\..\client_packages\cs_packages
