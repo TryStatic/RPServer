ba6c43aee77d2c6c2a4c23e318684eb1e294432a
Static <jimakosfb@gmail.com>
2019-06-16 04:06:38 +0300

    Added an Init class to be used as the main entry point for the script
    Extends Script from NAPI and implements the ResourceStart event with some initial settings

---
+++ bridge/resources/RPServer/_init/Init.cs
@@ -0,0 +1,21 @@
+﻿using GTANetworkAPI;
+
+namespace RPServer._init
+{
+    internal class Init : Script
+    {
+        [ServerEvent(Event.ResourceStart)]
+        public void OnResourceStart()
+        {
+            // Server Settings
+            //NAPI.Server.SetAutoSpawnOnConnect(false);
+            //NAPI.Server.SetAutoRespawnAfterDeath(false);
+
+            // Sever World Settings
+            NAPI.World.SetTime(0, 0, 0);
+            NAPI.World.ResetIplList();
+
+
+        }
+    }
+}
