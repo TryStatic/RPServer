d2a848aa6518dce1cccd8911255e75ebf7b130df
Static <jimakosfb@gmail.com>
2019-06-16 03:59:09 +0300

    Added a basic Logger for keeping the Console Output structured
    Includes a couple of Logging Methods for MySQL Errors and MySQL Info messages

---
+++ bridge/resources/RPServer/Util/Logger.cs
@@ -0,0 +1,17 @@
+﻿using GTANetworkAPI;
+
+namespace RPServer.Util
+{
+    internal class Logger
+    {
+        public static void MySqlError(string errorMsg, uint errorCode)
+        {
+            NAPI.Util.ConsoleOutput($"[MySQL Error]: {errorMsg} [Code={errorCode}]");
+        }
+
+        public static void MySqlInfo(string msg)
+        {
+            NAPI.Util.ConsoleOutput($"[MySQL Info]: {msg}");
+        }
+    }
+}
