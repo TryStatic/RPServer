658c82e98740d2241745c64578cdb9428bbac501
Static <jimakosfb@gmail.com>
2019-06-19 00:59:49 +0300

    Added Debug as a utility class with PrintObjectInfo(...) method
    The class is intended for keeping debugging related methods. PrintObjectInfo(...) prints all properties with their values of the referenced class.

---
+++ bridge/resources/RPServer/Util/Debug.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace RPServer.Util
+{
+    internal static class Debug
+    {
+        public static void PrintObjectInfo(object o)
+        {
+            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(o))
+            {
+                var name = descriptor.Name;
+                var value = descriptor.GetValue(o);
+                Console.WriteLine(@"{0}={1}", name, value);
+            }
+            Console.WriteLine("================================================");
+        }
+    }
+}
