2b0bda0949785518e03113c83be6f8a06c1d0ee6
Static <jimakosfb@gmail.com>
2019-06-19 00:56:08 +0300

    Added PasswordHash utility intended for hashing and salting passwords

---
+++ bridge/resources/RPServer/Util/PasswordHash.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace RPServer.Util
+{
+    internal sealed class PasswordHash
+    {
+        private const int SaltSize = 20;
+        private const int HashSize = 30;
+        private const int HashIter = 10000;
+
+        private readonly byte[] _salt;
+        private readonly byte[] _hash;
+
+        public byte[] Salt => (byte[])_salt.Clone();
+        public byte[] Hash => (byte[])_hash.Clone();
+
+        public PasswordHash(string password)
+        {
+            new RNGCryptoServiceProvider().GetBytes(_salt = new byte[SaltSize]);
+            _hash = new Rfc2898DeriveBytes(password, _salt, HashIter).GetBytes(HashSize);
+        }
+
+
+        public PasswordHash(byte[] hashBytes)
+        {
+            Array.Copy(hashBytes, 0, _salt = new byte[SaltSize], 0, SaltSize);
+            Array.Copy(hashBytes, SaltSize, _hash = new byte[HashSize], 0, HashSize);
+        }
+
+        public PasswordHash(byte[] salt, byte[] hash)
+        {
+            Array.Copy(salt, 0, _salt = new byte[SaltSize], 0, SaltSize);
+            Array.Copy(hash, 0, _hash = new byte[HashSize], 0, HashSize);
+        }
+
+        public byte[] ToArray()
+        {
+            byte[] hashBytes = new byte[SaltSize + HashSize];
+            Array.Copy(_salt, 0, hashBytes, 0, SaltSize);
+            Array.Copy(_hash, 0, hashBytes, SaltSize, HashSize);
+            return hashBytes;
+        }
+
+        public bool Verify(string password)
+        {
+            byte[] test = new Rfc2898DeriveBytes(password, _salt, HashIter).GetBytes(HashSize);
+            for (int i = 0; i < HashSize; i++)
+                if (test[i] != _hash[i])
+                    return false;
+            return true;
+        }
+    }
+
+}
