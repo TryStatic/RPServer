35953c6cdb29423c89fc72328123e7df36df2bc2
Static <jimakosfb@gmail.com>
2019-06-19 01:35:48 +0300

    Utility class for Extension methods

---
+++ bridge/resources/RPServer/Util/StringExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using MySql.Data.MySqlClient;
+
+namespace RPServer.Util
+{
+    public static class StringExtensions
+    {
+        public static string FirstCharToUpper(this string input)
+        {
+            switch (input)
+            {
+                case null: throw new ArgumentException(nameof(input));
+                case "": throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input));
+                default: return char.IsUpper(input.First()) ? input : input.First().ToString().ToUpper() + input.Substring(1);
+            }
+        }
+
+        public static bool IsValidEmail(this string emailString)
+        {
+            return new EmailAddressAttribute().IsValid(emailString);
+        }
+
+        public static string GetSafeString(this MySqlDataReader reader, string column)
+        {
+            return !reader.IsDBNull(reader.GetOrdinal(column)) ? reader.GetString(column) : null;
+        }
+
+        public static DateTime GetSafeDateTime(this MySqlDataReader reader, string column)
+        {
+            return !reader.IsDBNull(reader.GetOrdinal(column)) ? reader.GetDateTime(column) : DateTime.MinValue;
+        }
+   
+    }
+}
