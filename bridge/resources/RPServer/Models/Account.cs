e1ae4b808ecc836155d89568c53d5b652500cbb8
Static <jimakosfb@gmail.com>
2019-06-19 01:13:42 +0300

    Added the model for the Account with it's initial properties

---
+++ bridge/resources/RPServer/Models/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RPServer.Models
+{
+    internal class Account
+    {
+        public int? SqlId { get; private set; }
+        public string Username { get; set; }
+        public string EmailAddress { get; set; }
+        public byte[] Hash { get; set; }
+        public string ForumName { get; set; }
+        public string NickName { get; set; }
+        public string RegSocialClubName { get; set; }
+        public string LastSocialClubName { get; set; }
+        public string LastIP { get; set; }
+        public string LastHWID { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime LastLoginDate { get; set; }
+
+        private Account()
+        {
+
+        }
+
+    }
+}
