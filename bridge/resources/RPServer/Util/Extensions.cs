f7877dd3a651bae28fb73307aa70ff9d1682d38f
Static <jimakosfb@gmail.com>
2019-06-20 20:34:58 +0300

    Added regions within the Extensions class for readability.
    Introduced region for Client Extensions

--- bridge/resources/RPServer/Util/Extensions.cs
+++ bridge/resources/RPServer/Util/Extensions.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using GTANetworkAPI;
 using MySql.Data.MySqlClient;
+using RPServer.Models;
 
 namespace RPServer.Util
 {
     public static class Extensions
     {
+        #region StringExtensions
+
         public static string FirstCharToUpper(this string input)
         {
             switch (input)
@@ -22,6 +26,9 @@ namespace RPServer.Util
             return new EmailAddressAttribute().IsValid(emailString);
         }
 
+        #endregion
+
+        #region MySQLExtensions
         public static string GetSafeString(this MySqlDataReader reader, string column)
         {
             return !reader.IsDBNull(reader.GetOrdinal(column)) ? reader.GetString(column) : null;
@@ -31,5 +38,11 @@ namespace RPServer.Util
         {
             return !reader.IsDBNull(reader.GetOrdinal(column)) ? reader.GetDateTime(column) : DateTime.MinValue;
         }
+
+        #endregion
+
+        #region ClientExtensions
+
+        #endregion
     }
 }
