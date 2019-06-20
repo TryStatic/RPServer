f0166c9472c3fdb88504754913df19d5ee62de05
Static <jimakosfb@gmail.com>
2019-06-20 21:07:36 +0300

    Introduction of the Account Manager controller

---
+++ bridge/resources/RPServer/Controllers/AccountManager.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using GTANetworkAPI;
+using RPServer.Models;
+using RPServer.Util;
+using static RPServer.Util.DataValidator;
+
+namespace RPServer.Controllers
+{
+    internal class AccountManager : Script
+    {
+        public static void RegisterNewAccount(Client client, string username, string password)
+        {
+            if (!ValidateString(ValidationStrings.Username, username))
+            {
+                client.SendChatMessage("Username too short, 4 chars min"); // TODO: Implement proper chat handler
+                return;
+            }
+
+            if (!ValidateString(ValidationStrings.Password, password))
+            {
+                client.SendChatMessage("Password too short, 4 chars min");
+                return;
+            }
+
+            var newAcc = Account.Create(username, password, client.SocialClubName);
+
+            // Auto-login after registration
+            client.Login(newAcc);
+            client.SendChatMessage("You have automatically logged in. You now need to verify your email address");
+
+        }
+
+
+        public static void LoginAccount(Client client, string username, string password)
+        {
+            if (!ValidateString(ValidationStrings.Username, username))
+            {
+                client.SendChatMessage("Username too short, 4 chars min"); // TODO: Implement proper chat handler
+                return;
+            }
+
+            if (!ValidateString(ValidationStrings.Password, password))
+            {
+                client.SendChatMessage("Password too short, 4 chars min");
+                return;
+            }
+
+            if (!Account.Authenticate(username, password))
+            {
+                client.SendChatMessage("Wrong pass, try again");
+                return;
+            }
+
+            // Authentication succeeded
+            var fetchedAcc = Account.Fetch(username);
+            client.Login(fetchedAcc);
+
+            if (EmailToken.Exists(fetchedAcc))
+            {
+                client.SendChatMessage("You haven't verified your account yet.");
+                return;
+            }
+
+            // Email verification already happened
+            // TODO: Toggle logging in screen off
+
+        }
+
+        public static void VerifyEmail(Client client, string providedToken)
+        {
+            if (!client.IsLoggedIn())
+            {
+                client.SendChatMessage("You must be logged in to verify your email address.");
+                return;
+            }
+
+            if (!ValidateString(ValidationStrings.EmailVerificationCode, providedToken))
+            {
+                client.SendChatMessage("Invlid Code Length");
+                return;
+            }
+
+            if (!EmailToken.Validate(client.GetAccountData(), providedToken))
+            {
+                client.SendChatMessage("The code you provided is wrong, try again.");
+                return;
+            }
+
+            // Success, when EmailToken.Validate(..) return true the entry from EmailTokens is already removed.
+            client.SendChatMessage("Email Verificaton passed. Go play...");
+
+            // TODO: Toggle logging in screen off
+
+        }
+
+
+
+
+    }
+}
