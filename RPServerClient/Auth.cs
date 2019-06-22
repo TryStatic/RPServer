683c2bbe6d16d525a5e8907a34918e17a0a0f3ad
Static <jimakosfb@gmail.com>
2019-06-22 06:28:41 +0300

    Added a class to handle Authentication for the Client

---
+++ RPServerClient/Auth.cs
@@ -0,0 +1,31 @@
+﻿using RAGE;
+using RAGE.Elements;
+using RAGE.Ui;
+
+namespace RPServerClient
+{
+    internal class Auth : Events.Script
+    {
+        public Auth()
+        {
+            Events.Add("SetLoginScreen", OnSetLoginScreen);
+        }
+
+        private void OnSetLoginScreen(object[] args)
+        {
+            var state = (bool)args[0];
+
+            Player.LocalPlayer.FreezePosition(state);
+
+            if (state)
+            {
+                RAGE.Game.Graphics.TransitionToBlurred(200);
+
+            }
+            else
+            {
+                RAGE.Game.Graphics.TransitionFromBlurred(200);
+            }
+        }
+    }
+}
