f4a83a2dff07aeafaf6db515a9b2413b6e7c3e1c
Static <jimakosfb@gmail.com>
2019-06-27 02:52:13 +0300

    Moved PasswordHash class within the Account.cs
    It's only used for accounts makes sense to keep them together

--- bridge/resources/RPServer/Models/Account.cs
+++ bridge/resources/RPServer/Models/Account.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Data;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using RPServer.Database;
@@ -299,4 +300,54 @@ namespace RPServer.Models
             return !Equals(left, right);
         }
     }
+
+    internal sealed class PasswordHash
+    {
+        private const int SaltSize = 20;
+        private const int HashSize = 30;
+        private const int HashIter = 10000;
+
+        private readonly byte[] _salt;
+        private readonly byte[] _hash;
+
+        public byte[] Salt => (byte[])_salt.Clone();
+        public byte[] Hash => (byte[])_hash.Clone();
+
+        public PasswordHash(string password)
+        {
+            new RNGCryptoServiceProvider().GetBytes(_salt = new byte[SaltSize]);
+            _hash = new Rfc2898DeriveBytes(password, _salt, HashIter).GetBytes(HashSize);
+        }
+
+
+        public PasswordHash(byte[] hashBytes)
+        {
+            Array.Copy(hashBytes, 0, _salt = new byte[SaltSize], 0, SaltSize);
+            Array.Copy(hashBytes, SaltSize, _hash = new byte[HashSize], 0, HashSize);
+        }
+
+        public PasswordHash(byte[] salt, byte[] hash)
+        {
+            Array.Copy(salt, 0, _salt = new byte[SaltSize], 0, SaltSize);
+            Array.Copy(hash, 0, _hash = new byte[HashSize], 0, HashSize);
+        }
+
+        public byte[] ToArray()
+        {
+            byte[] hashBytes = new byte[SaltSize + HashSize];
+            Array.Copy(_salt, 0, hashBytes, 0, SaltSize);
+            Array.Copy(_hash, 0, hashBytes, SaltSize, HashSize);
+            return hashBytes;
+        }
+
+        public bool Verify(string password)
+        {
+            byte[] test = new Rfc2898DeriveBytes(password, _salt, HashIter).GetBytes(HashSize);
+            for (int i = 0; i < HashSize; i++)
+                if (test[i] != _hash[i])
+                    return false;
+            return true;
+        }
+    }
+
 }
