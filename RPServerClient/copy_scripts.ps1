d8645104e13b6a18638791cf4ccb5c40ff30a336
Static <jimakosfb@gmail.com>
2019-06-27 09:36:09 +0300

    Updated PoWeRsHeLL to copy shared resources to cs_packages

--- RPServerClient/copy_scripts.ps1
+++ RPServerClient/copy_scripts.ps1
@@ -1,3 +1,4 @@
 ﻿New-Item -ItemType Directory -Force -Path $PSScriptRoot\..\client_packages\cs_packages *>&1 | out-null
 Remove-Item -Path $PSScriptRoot\..\client_packages\cs_packages\* -Recurse
 get-childitem -path $PSScriptRoot -exclude obj, bin | Get-Childitem -recurse | Where-Object {$_.Extension -eq ".cs"} | Copy-Item -Destination $PSScriptRoot\..\client_packages\cs_packages
+get-childitem -path $PSScriptRoot\..\SharedProject -exclude obj, bin | Get-Childitem -recurse | Where-Object {$_.Extension -eq ".cs"} | Copy-Item -Destination $PSScriptRoot\..\client_packages\cs_packages
