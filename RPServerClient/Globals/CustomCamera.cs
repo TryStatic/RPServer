e150c113d5e8659f5ce98c2a35cfce21eeb30e8f
Static <jimakosfb@gmail.com>
2019-06-30 07:20:25 +0300

    Added a basic custom camera class for the client

---
+++ RPServerClient/Globals/CustomCamera.cs
@@ -0,0 +1,28 @@
+﻿using RAGE;
+
+namespace RPServerClient.Globals
+{
+    internal class CustomCamera
+    {
+        private readonly int _cameraId;
+
+        public CustomCamera(Vector3 camCord, Vector3 pointAt)
+        {
+            if (camCord == null || pointAt == null) return;
+
+            _cameraId = RAGE.Game.Cam.CreateCamera(RAGE.Game.Misc.GetHashKey("DEFAULT_SCRIPTED_CAMERA"), true);
+            RAGE.Game.Cam.SetCamCoord(_cameraId, camCord.X, camCord.Y, camCord.Z);
+            RAGE.Game.Cam.PointCamAtCoord(_cameraId, pointAt.X, pointAt.Y, pointAt.Z);
+        }
+        public void SetActive()
+        {
+            RAGE.Game.Cam.SetCamActive(_cameraId, true);
+            RAGE.Game.Cam.RenderScriptCams(true, false, 0, true, false, 0);
+        }
+        public void SetInactive()
+        {
+            RAGE.Game.Cam.DestroyCam(_cameraId, true);
+            RAGE.Game.Cam.RenderScriptCams(false, false, 0, true, false, 0);
+        }
+    }
+}
