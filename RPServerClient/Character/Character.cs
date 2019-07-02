dbefbf76a81728c13dc9d210be51f0eda476e2a1
Static <jimakosfb@gmail.com>
2019-07-02 16:07:59 +0300

    Implementation for the initialization of the Character Customization

--- RPServerClient/Character/Character.cs
+++ RPServerClient/Character/Character.cs
@@ -23,25 +23,38 @@ namespace RPServerClient.Character
             Events.CallLocal("setChatState", true);
             RAGE.Game.Ui.DisplayHud(true);
             RAGE.Game.Ui.DisplayRadar(true);
+
+            // TODO: Teleport player into the world
         }
 
         private void OnInitCharSelection(object[] args)
         {
             Events.CallLocal("setChatState", true); // Enabled for testing TODO: needs to be removed
             var myPlayer = Player.LocalPlayer;
+            var myPosition = myPlayer.Position;
 
+            // Stage the model
             myPlayer.Position = new Vector3(-169.3321f, 482.2647f, 133.8789f);
             myPlayer.FreezePosition(true);
             myPlayer.SetHeading(282.6658f);
-            //myPlayer.SetAlpha(0, false);
-      
-            var forwardX = myPlayer.Position.X + myPlayer.GetForwardX() * 1.5f;
-            var forwardY = myPlayer.Position.Y + myPlayer.GetForwardY() * 1.5f;
-            var cameraPos = new Vector3(forwardX, forwardY, myPlayer.Position.Z + 0.5f);
-            _characterDisplayCamera = new CustomCamera(cameraPos, myPlayer.Position);
+            myPlayer.SetAlpha(0, false);
+            Events.CallRemote("ApplyCharSelectionAnimation");
+
+            // Camera
+            var cameraPos = GetCameraPosInFrontOfPlayer(myPlayer, 1.5f);
+            _characterDisplayCamera = new CustomCamera(cameraPos, myPosition);
             _characterDisplayCamera.SetActive(true);
 
-            Events.CallRemote("ApplyCharSelectionAnimation");
+            // Display the Browser UI
+            CustomBrowser.CreateBrowser("package://CEF/char/index.html");
+        }
+
+        private static Vector3 GetCameraPosInFrontOfPlayer(Player myPlayer, float range)
+        {
+            var forwardX = myPlayer.Position.X + myPlayer.GetForwardX() * range;
+            var forwardY = myPlayer.Position.Y + myPlayer.GetForwardY() * range;
+            var cameraPos = new Vector3(forwardX, forwardY, myPlayer.Position.Z + 0.5f);
+            return cameraPos;
         }
     }
 }
