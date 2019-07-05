12cc6e6029720c6e9554db0b0c1c010f09e4cff2
Vladimir Kincl <kincl.vlada@gmail.com>
2019-07-06 01:11:15 +0200

    It seems like the Json include got lost in the process in one of the files.

--- bridge/resources/RPServer/Models/Character.cs
+++ bridge/resources/RPServer/Models/Character.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Data.Common;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using RPServer.Database;
 using RPServer.Models.CharacterHelpers;
 using RPServer.Models.Helpers;
