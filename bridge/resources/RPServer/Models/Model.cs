afa4958c7791e339649bd3c7c0e7fbc981bda599
Static <jimakosfb@gmail.com>
2019-07-08 01:03:25 +0300

    Gave the Base class it's own file and renamed it to Model

---
+++ bridge/resources/RPServer/Models/Model.cs
@@ -0,0 +1,81 @@
+﻿using System.Data.Common;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+using RPServer.Database;
+
+namespace RPServer.Models
+{
+    internal class Model<T> where T: class
+    {
+        public static async Task<int> CreateAsync(T newEntry)
+        {
+            using (var dbConn = DbConnectionProvider.CreateDbConnection())
+            {
+                try
+                {
+                    return await dbConn.InsertAsync(newEntry);
+                }
+                catch (DbException ex)
+                {
+                    DbConnectionProvider.HandleDbException(ex);
+                }
+
+                return -1;
+            }
+        }
+        public async Task<int> CreateAsync() => await CreateAsync(this as T);
+
+        public static async Task<T> ReadAsync(int sqlID)
+        {
+            using (var dbConn = DbConnectionProvider.CreateDbConnection())
+            {
+                try
+                {
+                    return await dbConn.GetAsync<T>(sqlID);
+                }
+                catch (DbException ex)
+                {
+                    DbConnectionProvider.HandleDbException(ex);
+                }
+
+                return null;
+            }
+        }
+
+        public static async Task<bool> UpdateAsync(T dbAcc)
+        {
+            using (var dbConn = DbConnectionProvider.CreateDbConnection())
+            {
+                try
+                {
+                    return await dbConn.UpdateAsync(dbAcc);
+                }
+                catch (DbException ex)
+                {
+                    DbConnectionProvider.HandleDbException(ex);
+                }
+
+                return false;
+            }
+        }
+        public async Task<bool> UpdateAsync() => await UpdateAsync(this as T);
+
+        public static async Task<bool> DeleteAsync(T dbAcc)
+        {
+            using (var dbConn = DbConnectionProvider.CreateDbConnection())
+            {
+                try
+                {
+                    return await dbConn.DeleteAsync(dbAcc);
+                }
+                catch (DbException ex)
+                {
+                    DbConnectionProvider.HandleDbException(ex);
+                }
+
+                return false;
+            }
+        }
+        public async Task<bool> DeleteAsync() => await DeleteAsync(this as T);
+    }
+}
