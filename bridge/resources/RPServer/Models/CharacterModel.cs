c28cfaf0f549f45d06e54451d1a47629fcd9cad2
Static <jimakosfb@gmail.com>
2019-07-08 01:14:05 +0300

    CharacterModel now inherits from Model

--- bridge/resources/RPServer/Models/CharacterModel.cs
+++ bridge/resources/RPServer/Models/CharacterModel.cs
@@ -9,7 +9,7 @@ using RPServer.Database;
 namespace RPServer.Models
 {
     [Table("characters")]
-    internal class CharacterModel
+    internal class CharacterModel : Model<CharacterModel>
     {
         [Key]
         public int CharacterID { get; set; }
@@ -26,41 +26,6 @@ namespace RPServer.Models
             CharacterName = name;
         }
 
-
-        public static async Task<int> CreateAsync(CharacterModel newData)
-        {
-            using (var dbConn = DbConnectionProvider.CreateDbConnection())
-            {
-                try
-                {
-                    return await dbConn.InsertAsync(newData);
-                }
-                catch (DbException ex)
-                {
-                    DbConnectionProvider.HandleDbException(ex);
-                }
-
-                return -1;
-            }
-        }
-        public async Task<int> CreateAsync() => await CreateAsync(this);
-
-        public static async Task<CharacterModel> ReadAsync(int sqlID)
-        {
-            using (var dbConn = DbConnectionProvider.CreateDbConnection())
-            {
-                try
-                {
-                    return await dbConn.GetAsync<CharacterModel>(sqlID);
-                }
-                catch (DbException ex)
-                {
-                    DbConnectionProvider.HandleDbException(ex);
-                }
-
-                return null;
-            }
-        }
         public static async Task<List<CharacterModel>> ReadByAccountAsync(Account account)
         {
             const string query = "SELECT * FROM characters WHERE CharOwnerID = @accountID";
@@ -82,42 +47,6 @@ namespace RPServer.Models
             }
             return null;
         }
-
-        public static async Task<bool> UpdateAsync(CharacterModel dbAcc)
-        {
-            using (var dbConn = DbConnectionProvider.CreateDbConnection())
-            {
-                try
-                {
-                    return await dbConn.UpdateAsync(dbAcc);
-                }
-                catch (DbException ex)
-                {
-                    DbConnectionProvider.HandleDbException(ex);
-                }
-
-                return false;
-            }
-        }
-        public async Task<bool> UpdateAsync() => await UpdateAsync(this);
-
-        public static async Task<bool> DeleteAsync(CharacterModel dbAcc)
-        {
-            using (var dbConn = DbConnectionProvider.CreateDbConnection())
-            {
-                try
-                {
-                    return await dbConn.DeleteAsync(dbAcc);
-                }
-                catch (DbException ex)
-                {
-                    DbConnectionProvider.HandleDbException(ex);
-                }
-
-                return false;
-            }
-        }
-        public async Task<bool> DeleteAsync() => await DeleteAsync(this);
 
     }
 }
