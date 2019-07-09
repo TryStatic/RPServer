42165b971e4ff14c19d4efe4ed243c1fa24bf36e
Static <jimakosfb@gmail.com>
2019-07-09 08:15:41 +0300

    Added empty ChatHandler Controller Class to host chat related operations

---
+++ bridge/resources/RPServer/Controllers/ChatHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace RPServer.Controllers
+{
+    internal class ChatHandler
+    {
+        // TODO: Add commands and methods for chat related stuff (local/faction w/e chat etc)
+        // TODO: This should work in combination with the PlayerChat Event Handler
+    }
+}
