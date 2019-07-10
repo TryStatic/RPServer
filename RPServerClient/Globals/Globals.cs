ff0c1cce6868c716ebb137532d0529196b355694
Static <jimakosfb@gmail.com>
2019-07-11 02:17:14 +0300

    Updated Client's Globals class to include the Active Char ID

--- RPServerClient/Globals/Globals.cs
+++ RPServerClient/Globals/Globals.cs
@@ -5,6 +5,9 @@ namespace RPServerClient.Globals
 {
     internal class Globals : RAGE.Events.Script
     {
-        public static bool IsAccountLoggedIn => (bool) Player.LocalPlayer.GetSharedData(SharedDataKey.AccountLoggedIn);
+        public static bool IsAccountLoggedIn => (bool)Player.LocalPlayer.GetSharedData(SharedDataKey.AccountLoggedIn);
+
+        public static bool HasActiveChar => (int)Player.LocalPlayer.GetSharedData(SharedDataKey.ActiveCharID) > 0;
+        public static int GetActiveCharID => (int)Player.LocalPlayer.GetSharedData(SharedDataKey.ActiveCharID);
     }
 }
