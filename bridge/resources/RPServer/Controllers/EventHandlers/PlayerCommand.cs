33f06861842f22e7622ab11e62bdf0d6a4a20aca
Static <jimakosfb@gmail.com>
2019-07-10 07:25:28 +0300

    Included OnPlayerCommand to EventHandlers directory

---
+++ bridge/resources/RPServer/Controllers/EventHandlers/PlayerCommand.cs
@@ -0,0 +1,21 @@
+﻿using GTANetworkAPI;
+using RPServer.Util;
+using Shared;
+
+namespace RPServer.Controllers.EventHandlers
+{
+    internal class PlayerCommand : Script
+    {
+        [RemoteEvent(ClientToServer.SubmitPlayerCommand)]
+        public void ClientEvent_OnPlayerCommand(Client client, string cmd)
+        { // This CANNOT block commands
+            var username = "UNREGISTERED";
+            if (client.IsLoggedIn())
+            {
+                var accData = client.GetAccountData();
+                username = accData.Username;
+            }
+            Logger.GetInstance().CommandLog(username, cmd);
+        }
+    }
+}
--- bridge/resources/RPServer/Game/Initialization.cs
+++ bridge/resources/RPServer/Game/Initialization.cs
@@ -66,18 +66,6 @@ namespace RPServer.Game
             _expiredEmailTokensTimer = new Timer(OnRemoveExpiredEmailTokens, null, 1, Timeout.Infinite);
         }
 
-        [RemoteEvent(ClientToServer.SubmitPlayerCommand)]
-        public void ClientEvent_OnPlayerCommand(Client client, string cmd)
-        { // This CANNOT block commands
-            var username = "UNREGISTERED";
-            if (client.IsLoggedIn())
-            {
-                var accData = client.GetAccountData();
-                username = accData.Username;
-            }
-            Logger.GetInstance().CommandLog(username, cmd);
-        }
-
         private async void OnRemoveExpiredEmailTokens(object state)
         {
             await EmailToken.RemoveExpiredCodesAsync();
