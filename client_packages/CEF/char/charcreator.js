4e7b03e213c2a879901f7aa76734b3c63e1acd68
Static <jimakosfb@gmail.com>
2019-07-15 10:57:53 +0300

    Added Page/Step to get the face features for character in CEF

--- client_packages/CEF/char/charcreator.js
+++ client_packages/CEF/char/charcreator.js
@@ -1,25 +1,37 @@
+// Name / Gender
 var firstname = "";
 var lastname = "";
 var isMale = false;
+
+// HeadBlend
 var ShapeFirst = 0;
 var ShapeSecond = 0;
 var ShapeMix = 0.5;
 var SkinMix = 0.5;
 
+// FaceFeatures
+var FaceFeatures = new Float32Array(20);
+
+
+var Blemishes = 0;
+var BlemishesOpacity = 255;
+var BlemishesColor = 0;
+var BlemishesSecColor = 0;
 
 var currentStep;
 
 
 
 jQuery(function ($) {
-    ShowStep(2);
+    ShowStep(4);
 
-    $("#nextStep").click(function() {
+    $(".nextStep").click(function() {
+        console.log("called");
         ShowNextStep();
       });
 });
 
-
+// Gender/Name
 jQuery(function ($) {
     $("#creation_part1").on('submit', function (e) {
         e.preventDefault();
@@ -34,7 +46,7 @@ jQuery(function ($) {
     });
 });
 
-
+// Headblend
 $(function() {
     $("#ShapeFirst").ionRangeSlider({
         min: 0,
@@ -42,7 +54,8 @@ $(function() {
         from: 0,
         hide_min_max: true,
         onFinish: function (data) {
-
+            ShapeFirst = data["from"];
+            UpdateHeadBlend();
         }
     });
 
@@ -52,7 +65,8 @@ $(function() {
         from: 0,
         hide_min_max: true,
         onFinish: function (data) {
-            
+            ShapeSecond = data["from"];
+            UpdateHeadBlend();
         }
     });
 
@@ -60,10 +74,11 @@ $(function() {
         min: 0.0,
         max: 1.0,
         from: 0.5,
-        step: 0.1,
+        step: 0.01,
         hide_min_max: true,
         onFinish: function (data) {
-
+            ShapeMix = data["from"];
+            UpdateHeadBlend();
         }
     });
 
@@ -71,13 +86,93 @@ $(function() {
         min: 0.0,
         max: 1.0,
         from: 0.5,
-        step: 0.1,
+        step: 0.01,
         hide_min_max: true,
         onFinish: function (data) {
-
+            SkinMix = data["from"];
+            UpdateHeadBlend();
         }
     });
 });
+
+// FaceFeatures
+$(function() {
+    var i;
+    for(i=0;i<=19;i++) {
+        $("#facefeature" + i).ionRangeSlider({
+            min: -1.0,
+            max: 1.0,
+            from: 0.0,
+            step: 0.01,
+            hide_min_max: true,
+            onFinish: function (data) {
+                //FaceFeatures[i] = data["from"];
+                var id = $(data.input[0]).attr('id').match(/\d+/)[0];
+                FaceFeatures[id] = data["from"];
+                console.log(FaceFeatures[id]);
+                UpdateFaceFeature(id);
+            }
+        });
+    }
+
+});
+
+
+
+// HeadOverlay - Blemishes
+$(function() {
+    $("#Blemishes").ionRangeSlider({
+        min: 0,
+        max: 24,
+        from: 0,
+        hide_min_max: true,
+        onFinish: function (data) {
+            ShapeFirst = data["from"];
+            UpdateOverlay();
+        }
+    });
+    $("#BlemishesOpacity").ionRangeSlider({
+        min: 0.0,
+        max: 1.0,
+        from: 1.0,
+        step: 0.01,
+        hide_min_max: true,
+        onFinish: function (data) {
+            ShapeMix = data["from"];
+            UpdateOverlay();
+        }
+    });
+    $("#BlemishesColor").ionRangeSlider({
+        min: 0,
+        max: 24,
+        from: 0,
+        hide_min_max: true,
+        onFinish: function (data) {
+            ShapeFirst = data["from"];
+            UpdateOverlay();
+        }
+    });
+    $("#BlemishesSecColor").ionRangeSlider({
+        min: 0,
+        max: 24,
+        from: 0,
+        hide_min_max: true,
+        onFinish: function (data) {
+            ShapeFirst = data["from"];
+            UpdateOverlay();
+        }
+    });
+});
+
+function UpdateHeadBlend() {
+    //mp.trigger("onUpdateHeadBlend", ShapeFirst, ShapeSecond, SkinMix, ShapeMix, SkinMix);
+}
+
+function UpdateFaceFeature(index) {
+    //mp.trigger("onUpdateFaceFeature", index, FaceFeatures[index]);
+}
+
+
 
 function ShowNextStep() {
     currentStep++;
@@ -87,9 +182,9 @@ function ShowNextStep() {
 function ShowStep(numba) {
     currentStep = numba;
     $("#step" + numba).show();
-
+    console.log("showed"+currentStep);
     var i;
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < 100; i++) {
       if(i != numba) {
         $("#step" + i).hide();
       }
