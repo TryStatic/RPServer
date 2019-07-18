87c211c572fcc1cf184ce25ed604ab771d1bd104
Static <jimakosfb@gmail.com>
2019-07-18 20:17:58 +0300

    Turned the Character Listing on the CharSelector into a nativeUI list

--- RPServerClient/Character/CharSelector.cs
+++ RPServerClient/Character/CharSelector.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using Newtonsoft.Json;
 using RAGE;
 using RAGE.Elements;
+using RAGE.NUI;
 using RPServerClient.Globals;
 using RPServerClient.Util;
 using Shared;
@@ -12,6 +14,12 @@ namespace RPServerClient.Character
 {
     internal class CharSelector : Events.Script
     {
+        public static int ScreenX = 0;
+        public static int ScreenY = 0;
+
+        public static int ScreenResX = 0;
+        public static int ScreenResY = 0;
+
         private readonly Vector3 _displayPosition = new Vector3(-169.3321f, 482.2647f, 133.8789f);
         private readonly float _displayHeading = 282.6658f;
         private readonly Vector3 _hiddenPosition = new Vector3(-163.4660f, 483.5910f, 134.5571f);
@@ -19,12 +27,16 @@ namespace RPServerClient.Character
         private int _selectedCharId = -1;
         private List<CharDisplay> _charList = new List<CharDisplay>();
         private CustomCamera _characterDisplayCamera;
+        private MenuPool _charMenu;
 
         private bool _disableControls = false;
 
 
         public CharSelector()
         {
+            RAGE.Game.Graphics.GetScreenResolution(ref ScreenResX, ref ScreenResY);
+            RAGE.Game.Graphics.GetActiveScreenResolution(ref ScreenX, ref ScreenY);
+
             Events.Tick += Render;
 
             Events.Add(ServerToClient.InitCharSelector, OnInitCharSelector);
@@ -39,6 +51,7 @@ namespace RPServerClient.Character
         private void Render(List<Events.TickNametagData> nametags)
         {
             if (_disableControls) RAGE.Game.Pad.DisableAllControlActions(0);
+            _charMenu.ProcessMenus();
         }
 
         private void OnInitCharSelector(object[] args)
@@ -51,8 +64,6 @@ namespace RPServerClient.Character
 
             // Camera
             var cameraPos = Helper.GetPosInFrontOfVector3(_displayPosition, _displayHeading, 1.5f);
-            RAGE.Chat.Output(cameraPos.ToString());
-            RAGE.Chat.Output(_displayPosition.ToString());
             _characterDisplayCamera = new CustomCamera(cameraPos, _displayPosition);
             _characterDisplayCamera.SetActive(true);
             _disableControls = true;
@@ -100,12 +111,35 @@ namespace RPServerClient.Character
             _charList = JsonConvert.DeserializeObject<List<CharDisplay>>(args[0] as string);
             _selectedCharId = (int) args[1];
 
-            RAGE.Chat.Output("[CLIENT]: Your chars: ");
+            _charMenu = new MenuPool();
+            
+            var p = new Point(1350, 200);
+
+            var menu = new UIMenu("Char Select", "Select a character", p);
+            _charMenu.Add(menu);
+
             foreach (var c in _charList)
             {
-                RAGE.Chat.Output($"[CLIENT]: {c.CharID}, {c.CharName}");
+                var ch = new UIMenuItem($"{c.CharName}");
+                menu.AddItem(ch);
             }
-            RAGE.Chat.Output("[CLIENT]: -------------");
+
+            var createCharItem = new UIMenuColoredItem("Create New Character", Color.CadetBlue, Color.LightBlue);
+            menu.AddItem(createCharItem);
+
+            menu.OnItemSelect += (sender, item, index) =>
+            {
+                if (item == createCharItem)
+                {
+                    Events.CallLocal("createchar");
+                    return;
+                }
+
+                var selectedChar = _charList.Find(c => c.CharName == item.Text);
+                SelectChar(new object[] { selectedChar.CharID });
+            };
+
+            menu.Visible = true;
 
             if(_selectedCharId >= 0) SelectChar(new object[]{ _selectedCharId });
         }
