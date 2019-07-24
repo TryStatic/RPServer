495f10f4e612f9bccde0d618f2bca5d13974f070
Static <jimakosfb@gmail.com>
2019-07-24 11:39:27 +0300

    Started working on an AliasManager on Client

---
+++ RPServerClient/Character/AliasManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using RAGE;
+using RAGE.Elements;
+
+namespace RPServerClient.Character
+{
+    internal class AliasManager : Events.Script
+    {
+
+        public AliasManager()
+        {
+            Events.OnEntityStreamIn += OnPlayerStreamIn;
+            Events.OnEntityStreamOut += OnPlayerStreamOut;
+            Events.Tick += Tick;
+        }
+
+        private void OnPlayerStreamIn(Entity entity)
+        {
+            if (entity.Type != Type.Player) return;
+            var p = (Player) entity;
+
+            
+        }
+
+        private void OnPlayerStreamOut(Entity entity)
+        {
+            if (entity.Type != Type.Player) return;
+            var p = (Player)entity;
+
+        }
+
+
+        private void Tick(List<Events.TickNametagData> nametags)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
