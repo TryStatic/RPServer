4a9cf6cb7f0f57cebda8d68402b9ec10b6c9a777
Static <jimakosfb@gmail.com>
2019-07-26 05:49:12 +0300

    Added WorldData Save Timer every 5 minutes (it's just 1 db row)

--- bridge/resources/RPServer/Controllers/WorldHandler.cs
+++ bridge/resources/RPServer/Controllers/WorldHandler.cs
@@ -11,30 +11,42 @@ namespace RPServer.Controllers
         public static DateTime CurrentTime { set; get; }
 
         private static Timer _updateTimeTimer;
+        private static Timer _saveWorldDataTimer;
 
         public WorldHandler()
         {
             _updateTimeTimer = new Timer(OnUpdateTime, null, 0, 1000);
+            _saveWorldDataTimer = new Timer(OnSaveWorldData, null, 5000 * 60 * 5, 5000 * 60 * 5); // 5 minutes
         }
         
         public bool IsDayTime() => CurrentTime.Hour > 6 || CurrentTime.Hour < 21;
+        public static async Task OnServerShutdown()
+        {
+            Logger.GetInstance().ServerInfo("[SHUTDOWN]: Started saving World Data.");
+            // Save World Data
+            await SaveWorldData();
+            Logger.GetInstance().ServerInfo("[SHUTDOWN]: Finished saving World Data.");
+            // Dispose Timers
+            _updateTimeTimer.Dispose();
+            _saveWorldDataTimer.Dispose();
+        }
 
-        private void OnUpdateTime(object state)
+        private static void OnUpdateTime(object state)
         {
             CurrentTime = CurrentTime.AddSeconds(4.0);
             NAPI.World.SetTime(CurrentTime.Hour, CurrentTime.Minute, CurrentTime.Second);
         }
-
-        public static async Task OnServerShutdown()
-        { 
-            Logger.GetInstance().ServerInfo("[SHUTDOWN]: Started saving World Data.");
-            // Save World Data
+        private static async void OnSaveWorldData(object state)
+        {
+            await SaveWorldData();
+        }
+        private static async Task SaveWorldData()
+        {
             var worldData = await Models.World.GetWorldData();
+            if(worldData == null) return;
+            // Add world data here
             worldData.ServerTime = CurrentTime;
             await Models.World.SaveWorldData(worldData);
-            Logger.GetInstance().ServerInfo("[SHUTDOWN]: Finished saving World Data.");
-            // Dispose Timers
-            _updateTimeTimer.Dispose();
         }
     }
 }
