2c00c8d13f8252796c43dab8db6b7f97f1e6fa6b
Static <jimakosfb@gmail.com>
2019-07-26 06:39:53 +0300

    Renamed Events to EventsHandler on Client

--- RPServerClient/Client/EventsHandler.cs
+++ RPServerClient/Client/EventsHandler.cs
@@ -1,8 +1,8 @@
 ﻿namespace RPServerClient.Client
 {
-    internal class Events : RAGE.Events.Script
+    internal class EventsHandler : RAGE.Events.Script
     {
-        public Events()
+        public EventsHandler()
         {
             RAGE.Events.OnPlayerCommand += OnPlayerCommandEvent;
         }
