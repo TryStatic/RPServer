383dbfa80ffdcf6132dede1dcf6b5e7de3e20e3c
Static <jimakosfb@gmail.com>
2019-07-27 11:29:32 +0300

    Added Filter on OnChatMessage event to filter out HTML and Colors

--- bridge/resources/RPServer/Controllers/EventHandlers/PlayerChat.cs
+++ bridge/resources/RPServer/Controllers/EventHandlers/PlayerChat.cs
@@ -1,4 +1,6 @@
-﻿using GTANetworkAPI;
+﻿using System;
+using System.Text.RegularExpressions;
+using GTANetworkAPI;
 using RPServer.InternalAPI.Extensions;
 using RPServer.Util;
 
@@ -9,17 +11,26 @@ namespace RPServer.Controllers.EventHandlers
         [ServerEvent(Event.ChatMessage)]
         public void OnChatMessage(Client client, string message)
         {
-            if(!client.IsLoggedIn()) return;
+            if (!client.IsLoggedIn()) return;
+            if (!client.HasActiveChar()) return;
 
-            var acc = client.GetAccount();
-            var sendMsg = $"{acc.Username}: {message}";
+            message = Filter(message);
 
-            var ch = client.GetActiveChar();
-            if(client.GetActiveChar() != null) sendMsg = $"{client.GetActiveChar().CharacterName.Replace("_", " ")}: {message}";
-
-
+            var sendMsg = $"{client.GetActiveChar().CharacterName}: {message}";
             Logger.GetInstance().ChatLog(sendMsg);
             NAPI.ClientEvent.TriggerClientEventForAll("SendToChat", sendMsg);
+        }
+
+        public string Filter(string message)
+        {
+            if (message == null) return null;
+            if (message == "") return message;
+
+            message = System.Security.SecurityElement.Escape(message);
+            var matches = new Regex(@"(!{#[0-9A-F]{6}})+").Matches(message);
+            foreach (Match m in matches) message = message.Remove(message.IndexOf(m.Value, StringComparison.OrdinalIgnoreCase), 10);
+
+            return message;
         }
     }
 }
