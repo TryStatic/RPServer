018650fcac178c98d4e3c64fd274ebf5b0e93cd1
Static <jimakosfb@gmail.com>
2019-07-28 23:06:36 +0300

    Utilized character spawn/despawn events to set a chatmode for a client

---
+++ RPServerClient/Character/Character.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using RAGE;
+using RAGE.Game;
+
+namespace RPServerClient.Character
+{
+    internal enum ChatMode
+    {
+        NormalChat,
+        ShoutChat
+    }
+
+    internal class Character : Events.Script
+    {
+        public Character()
+        {
+            CharSelector.CharacterSpawn += OnCharacterSpawn;
+            CharSelector.CharacterDespawn += OnCharacterDespawn;
+        }
+
+        private void OnCharacterSpawn(object source, EventArgs e)
+        {
+            var player = source as RAGE.Elements.Player;
+
+            player?.SetData(RPServerClient.Util.LocalDataKeys.CurrentChatMode, ChatMode.NormalChat);
+        }
+
+        private void OnCharacterDespawn(object source, EventArgs e)
+        {
+            var player = source as Player;
+
+        }
+    }
+}
---
+++ RPServerClient/Util/LocalDataKeys.cs
@@ -0,0 +1,7 @@
+﻿namespace RPServerClient.Util
+{
+    internal static class LocalDataKeys
+    {
+        internal const string CurrentChatMode = "CURRENT_CHAT_MODE";
+    }
+}
