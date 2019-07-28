dc556229404233dac46d24a6dde226a0cb235f4a
Static <jimakosfb@gmail.com>
2019-07-29 00:04:30 +0300

    More move refactoring

--- RPServerClient/Client/Globals.cs
+++ RPServerClient/Client/Globals.cs
@@ -1,9 +1,15 @@
-﻿using RAGE.Elements;
+﻿using RAGE;
+using RAGE.Elements;
 
 namespace RPServerClient.Client
 {
-    internal static class Globals
+    public class Globals : Events.Script
     {
+        public Globals()
+        {
+            Nametags.Enabled = false;
+        }
+
         public static bool IsAccountLoggedIn => (bool)Player.LocalPlayer.GetSharedData(Shared.Data.Keys.AccountLoggedIn);
         public static bool HasActiveChar => (int)Player.LocalPlayer.GetSharedData(Shared.Data.Keys.ActiveCharID) > 0;
         public static int GetActiveCharID => (int)Player.LocalPlayer.GetSharedData(Shared.Data.Keys.ActiveCharID);
