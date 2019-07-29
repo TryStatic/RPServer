23f53e0c14664385e69d8905f2f0b186e6761cb7
Static <jimakosfb@gmail.com>
2019-07-29 03:46:24 +0300

    Get TRUE client screen resolution

---
+++ RPServerClient/Util/ScreenRes.cs
@@ -0,0 +1,32 @@
+﻿namespace RPServerClient.Util
+{
+    internal class ScreenRes : RAGE.Events.Script
+    {
+        public static int ScreenX
+        {
+            get
+            {
+                var screenX = 0;
+                var screenY = 0;
+                RAGE.Game.Graphics.GetActiveScreenResolution(ref screenX, ref screenY);
+                return screenX;
+            }
+        }
+
+        public static int ScreenY
+        {
+            get
+            {
+                var screenX = 0;
+                var screenY = 0;
+                RAGE.Game.Graphics.GetActiveScreenResolution(ref screenX, ref screenY);
+                return screenY;
+            }
+        }
+
+        public ScreenRes()
+        {
+
+        }
+    }
+}
