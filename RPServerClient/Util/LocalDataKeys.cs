25cef7e574765df83a321450bba0595c1f0c0918
Static <jimakosfb@gmail.com>
2019-07-31 07:21:44 +0300

    Not needed with the new Voice Implementation

--- RPServerClient/Util/LocalDataKeys.cs
+++ RPServerClient/Util/LocalDataKeys.cs
@@ -3,6 +3,5 @@
     internal static class LocalDataKeys
     {
         internal const string CurrentChatMode = "CURRENT_CHAT_MODE";
-        internal const string IsListener = "IS_LISTENING";
     }
 }
