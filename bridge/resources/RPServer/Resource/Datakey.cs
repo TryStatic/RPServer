600f39a769c05d07dbefe8b94b5eb4a6f739ea1a
Static <jimakosfb@gmail.com>
2019-08-05 14:03:52 +0300

    There are no longer needed

--- bridge/resources/RPServer/Resource/Datakey.cs
+++ bridge/resources/RPServer/Resource/Datakey.cs
@@ -4,9 +4,5 @@
     {
         public static readonly string AccountData = "ACCOUNT_DATA";
         public static readonly string ActiveCharData = "ACTIVE_CHARACTER_DATA";
-
-        public static readonly string CanRunTask = "CAN_RUN_TASK";
-        public static readonly string ActionQueue = "ACTION_QUEUE";
-        public static readonly string ActionQueueTimer = "ACTION_QUEUE_TIMER";
     }
 }
