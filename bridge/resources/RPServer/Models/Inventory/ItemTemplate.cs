3ef1a5a9e1f1a7be7b7cc3e58ed3d7a1bcf10843
Static <jimakosfb@gmail.com>
2019-09-20 05:15:39 +0300

    Stack types specified in the item template

--- bridge/resources/RPServer/Models/Inventory/ItemTemplate.cs
+++ bridge/resources/RPServer/Models/Inventory/ItemTemplate.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Reflection.Metadata.Ecma335;
+using System.Linq;
 using GTANetworkAPI;
 using RPServer.Controllers;
 using RPServer.InternalAPI.Extensions;
@@ -8,6 +8,13 @@ using RPServer.Util;
 
 namespace RPServer.Models.Inventory
 {
+    internal enum StackType
+    {
+        Singleton, // Only one instance of this ItemType per inventory and not stackable
+        Multiton, // Multiple instances of this ItemType per inventory and not stackable
+        Stackable // Only one instance of this ItemType per inventory but stackable
+    }
+
     /// <summary>
     /// Basic Template to characterize the various items
     /// </summary>
@@ -17,16 +24,25 @@ namespace RPServer.Models.Inventory
         private static int _lastUsedID;
 
         public int ID { get; }
-        public string ItemName { get; set; }
-        public string ItemDesc { get; set; }
+        public string ItemName { get; }
+        public string ItemDesc { get; }
+        public StackType StackType { get; }
 
-
-        public ItemTemplate(string itemName, string itemDesc)
+        private ItemTemplate(string itemName, string itemDesc, StackType stackType)
         {
             ID = _lastUsedID++;
             ItemName = itemName;
             ItemDesc = itemDesc;
+            StackType = stackType;
         }
+
+        /// <summary>
+        /// Searches for the corresponding ItemTemplate matching the specified template ID.
+        /// </summary>
+        /// <param name="id">The template ID</param>
+        /// <returns>Either the ItemTemplate instance or null if not found</returns>
+        public static ItemTemplate GetTemplate(uint id) => _itemTemplates.FirstOrDefault(it => it.ID == id);
+        public static List<ItemTemplate> GetTemplates() => _itemTemplates.ToList();
 
         #region ItemAction
         private Action<object[]> _itemAction;
@@ -50,10 +66,10 @@ namespace RPServer.Models.Inventory
 
             _itemTemplates = new HashSet<ItemTemplate>
             {
-                new ItemTemplate("Money", "The standard currency."),
-                new ItemTemplate("Dice", "Just an ordinary Dice")
+                new ItemTemplate("Money", "The standard currency.", StackType.Stackable),
+                new ItemTemplate("Dice", "Just an ordinary Dice", StackType.Multiton)
                 {
-                    _itemAction = args => // (client)
+                    _itemAction = args => // (Client client)
                     {
                         if (args == null || args.Length < 1) return;
                         var client = args[0] as Client;
@@ -64,13 +80,13 @@ namespace RPServer.Models.Inventory
 
                         ChatHandler.SendActionMessage(client, Shared.Data.Chat.NormalChatMaxDistance, $"rolls a dice landing on {RandomGenerator.GetInstance().Next(0, 7)}");
                     }
-                }
+                },
+                new ItemTemplate("Teddy Bear", "Just a Teddy Bear", StackType.Singleton)
             };
 
             Logger.GetInstance().ServerInfo($"\tLoaded {_itemTemplates.Count} item templates.");
 
         }
-
 
         #region HashcodeEquals
         protected bool Equals(ItemTemplate other)
