a636bb4cbc76008a82b7f595a6f07c39fe2ddf43
Static <jimakosfb@gmail.com>
2019-09-21 06:54:52 +0300

    DroppedItems class representation

---
+++ bridge/resources/RPServer/Models/Inventory/Inventory/DroppedItems.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using RPServer.Models.Inventory.Item;
+
+namespace RPServer.Models.Inventory.Inventory
+{
+    internal class DroppedItems
+    {
+        private readonly HashSet<DroppedItem> _droppedItems = new HashSet<DroppedItem>();
+
+    }
+}
