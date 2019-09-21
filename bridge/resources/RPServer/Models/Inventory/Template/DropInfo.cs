e904c2851075491bddc7655c9cccb409ca3ff99d
Static <jimakosfb@gmail.com>
2019-09-21 06:07:55 +0300

    Added DropInfo to specify the static details of a Droppable Item

---
+++ bridge/resources/RPServer/Models/Inventory/Template/DropInfo.cs
@@ -0,0 +1,16 @@
+﻿using GTANetworkAPI;
+
+namespace RPServer.Models.Inventory.Template
+{
+    internal class DropInfo
+    {
+        public DropInfo(uint objectID, Vector3 defaultRotation)
+        {
+            ObjectID = objectID;
+            DefaultRotation = defaultRotation;
+        }
+
+        public uint ObjectID { get; }
+        public Vector3 DefaultRotation { get; }
+    }
+}
