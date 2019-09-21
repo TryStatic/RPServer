66f621fa541446aecd0624987b61f776f37f7242
Static <jimakosfb@gmail.com>
2019-09-21 22:58:54 +0300

    Lowered the shout distance to a more reasonable value

--- SharedProject/Data/Chat.cs
+++ SharedProject/Data/Chat.cs
@@ -4,6 +4,6 @@
     {
         public const float LowChatMaxDistance = 5f;
         public const float NormalChatMaxDistance = 15f;
-        public const float ShoutChatMaxDistance = 45f;
+        public const float ShoutChatMaxDistance = 25f;
     }
 }
